def4286e67532519503796caf92c0f62dd6a07eb
Lilium <19245343+EndlesslyFlowering@users.noreply.github.com>
2023-01-29 13:05:57 +0100

    [d3d11] add helper for render target upgrades

---
+++ src/d3d11/d3d11_rt_upgrade_helper.h
@@ -0,0 +1,41 @@
+#include "d3d11_device.h"
+#include <dxgiformat.h>
+
+namespace dxvk {
+
+  class D3D11Device;
+
+  class D3D11RTUpgradeHelper {
+
+  public:
+
+    DXGI_FORMAT TranslateFormatString(
+      DXGI_FORMAT inputFormat,
+      std::string targetFormatString);
+
+    DXGI_FORMAT UpgradeRenderTarget(
+      DXGI_FORMAT Format);
+
+    std::string GetDXGIFormatNameAsString(
+      DXGI_FORMAT Format);
+
+    struct FormatInfo
+    {
+      std::string type             = "unknown";
+      uint32_t    activeComponents = 0;
+      std::string channelStructure = "";
+      uint32_t    depth0           = 0;
+      uint32_t    depth1           = 0;
+      uint32_t    depth2           = 0;
+      uint32_t    depth3           = 0;
+    };
+
+  private:
+  
+    D3D11Device* m_device;
+
+    std::string GetDXGIFormatInfo(
+      DXGI_FORMAT Format);
+
+  };
+}
